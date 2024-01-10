72963a72150284e3f7606ac526f37fb5b4d1b4d8
Roma <isaevramiz73@gmail.com>
2024-01-10 21:17:19 +0300

    start task

---
+++ input-output.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main():
+    name = input()
+    age = input()
+    country = input()
+    print(f'Уважаемый {name}, на данный момент Вы проживаете в: {country} и Вам {age}.')
+
+
+if __name__ == '__main__':
+    main()
